10582771eda944b4fbe4126a40baae08d4f0a9bd
Austin Feller <afeller08@gmail.com>
2014-06-28 21:06:41 -0500

    Create readme.

---
+++ divisibility.py
@@ -0,0 +1,9 @@
+"""
+Answer the question does m divide n in linear time with respect to
+the numerator.
+
+Comments should give enough clarity that the proof can be easily
+completed from reading.
+"""
+
+
